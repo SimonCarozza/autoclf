0cadeb57ea5a231f74589f50172f47c8d3407abb
SimonCarozza <45878201+SimonCarozza@users.noreply.github.com>
2019-01-17 17:46:56 +0100

    Update getargs.__init__

--- autoclf/getargs/__init__.py
+++ autoclf/getargs/__init__.py
@@ -1,16 +1,4 @@
-import sys
-sys.path.append("C:\\PROGRAMMAZIONE\\Sviluppo\\AI\\MachineLearning\\Projects\\autoclf")
 import argparse
-
-# class FooAction(argparse.Action):
-#     def __init__(self, option_strings, dest, nargs=None, **kwargs):
-#         if nargs is not None:
-#             raise ValueError("nargs not allowed")
-#         super(FooAction, self).__init__(option_strings, dest, **kwargs)
-#     def __call__(self, parser, namespace, values, option_string=None):
-#         print('%r %r %r' % (namespace, values, option_string))
-#         setattr(namespace, self.dest, values)
-
 
 parser = argparse.ArgumentParser(prog="Autoclf",
     description="Get experiment's arguments.")
