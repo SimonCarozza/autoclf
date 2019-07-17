65f97a0646a2586128ea9c4881244ccfa0cfe864
SimonCarozza <simoncarozza@gmail.com>
2019-07-17 17:49:46 +0200

    Update getargs init to avoid jupyter breaks at import

--- autoclf/getargs/__init__.py
+++ autoclf/getargs/__init__.py
@@ -31,7 +31,7 @@ parser.add_argument("--npset", nargs=1, type=int,
     help="Number of number of parameter settings sampled by RSCV")
 parser.add_argument("--nepoch", nargs="+", type=int,
     help="Number of epochs to train Keras NNs")
-args = parser.parse_args()
+args = parser.parse_args([])
 
 
 def get_args():
